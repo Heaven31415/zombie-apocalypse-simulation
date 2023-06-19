e581858da54ddc13a22caa8e81335c42bddb2836
Heaven31415 <heaven31415@gmail.com>
2023-06-19 13:31:43 +0200

    Add ZombieController

---
+++ src/Controller/ZombieController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Zombie;
+use App\Form\ZombieType;
+use App\Repository\ZombieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ZombieController extends AbstractController
+{
+    #[Route('/zombie/new', name: 'app_new_zombie')]
+    public function new(Request $request, ZombieRepository $zombieRepository): Response
+    {
+        $zombie = new Zombie();
+
+        $zombie->setX(0)
+            ->setY(0);
+
+        $form = $this->createForm(ZombieType::class, $zombie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $zombie = $form->getData();
+
+            $zombieRepository->save($zombie, true);
+
+            $this->addFlash('notice', 'A new zombie has been successfully created!');
+
+            return $this->redirectToRoute('app_simulation');
+        }
+
+        return $this->render('zombie/new.html.twig', ['form' => $form]);
+    }
+}
